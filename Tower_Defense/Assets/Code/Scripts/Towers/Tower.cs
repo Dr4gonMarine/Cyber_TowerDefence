a655c8270f0342c03310f55da48b07cc60d58cf2
Dr4gonMarine <65034442+Dr4gonMarine@users.noreply.github.com>
2023-06-01 14:57:05 -0300

    Ajuste no script de torre

--- Tower_Defense/Assets/Code/Scripts/Towers/Tower.cs
+++ Tower_Defense/Assets/Code/Scripts/Towers/Tower.cs
@@ -6,53 +6,24 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] TowerSettings _towerSettings;
-    [SerializeField] List<Enemy> _enemiesInRange;
+  
     
     public float Range {get => _towerSettings.Range; }
     public float FireRate {get => _towerSettings.FireRate; }
     public int NumDDosEffects = 0;
-    Enemy CurrentEnemy;    
 
     private void Awake()
-    {
-        CircleCollider2D range = GetComponent<CircleCollider2D>();
+    {       
+        CircleCollider2D range = GetComponentInChildren<CircleCollider2D>();
         range.radius = Range;
     }
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    
+ 
+    private void OnMouseDown()
     {
-        if (collision.CompareTag("Enemy"))
-        {
-            _enemiesInRange.Add(collision.GetComponent<Enemy>());
-        }
+        Debug.Log("Tower Clicked");
     }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Enemy"))
-        {
-            Enemy enemy = collision.GetComponent<Enemy>();
-            if (_enemiesInRange.Contains(enemy))
-                _enemiesInRange.Remove(enemy);
-        }
-    }   
-
-    void Update()
-    {
-        GetCurrentEnemyTarget();
-    }   
-
-    public Enemy  GetCurrentEnemyTarget()
-    {
-        if(_enemiesInRange.Count <= 0)
-        {
-            CurrentEnemy = null;
-            return null;
-        }
-        CurrentEnemy = _enemiesInRange[0];
-        return CurrentEnemy;
-    }
+ 
 
     public void AddDDosEffect()
     {
--- Tower_Defense/Assets/Code/Scripts/Towers/TowerProjectileController.cs
+++ Tower_Defense/Assets/Code/Scripts/Towers/TowerProjectileController.cs
@@ -3,34 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerProjectileController : MonoBehaviour
-{    
+{
     private Tower _tower;
     private ObjectPooler _pool;
 
-    protected float _nextAttkTime;       
-    Projectile _currentProjectileLoaded;   
-    
+    Enemy CurrentEnemy;
+    [SerializeField] List<Enemy> _enemiesInRange;
+
+    protected float _nextAttkTime;
+    Projectile _currentProjectileLoaded;
+
     private void Awake()
     {
-        _tower = GetComponent<Tower>();
+        _tower = GetComponentInParent<Tower>();
         _pool = GetComponent<ObjectPooler>();
     }
-
     void Update()
     {
-        if (_tower.GetCurrentEnemyTarget() != null && _tower.NumDDosEffects == 0)
+        if (GetCurrentEnemyTarget() != null && _tower.NumDDosEffects == 0)
             LoadProjectiles();
+    }
+
+    public Enemy GetCurrentEnemyTarget()
+    {
+        if (_enemiesInRange.Count <= 0)
+        {
+            CurrentEnemy = null;
+            return null;
+        }
+        CurrentEnemy = _enemiesInRange[0];
+        return CurrentEnemy;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            _enemiesInRange.Add(collision.GetComponent<Enemy>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            if (_enemiesInRange.Contains(enemy))
+                _enemiesInRange.Remove(enemy);
+        }
     }
 
     void LoadProjectiles()
     {
-        if(Time.time > _nextAttkTime)
+        if (Time.time > _nextAttkTime)
         {
             GameObject newInstace = _pool.GetInstaceFromPool();
             newInstace.SetActive(true);
             _currentProjectileLoaded = newInstace.GetComponent<Projectile>();
             _currentProjectileLoaded.transform.position = transform.position;
-            _currentProjectileLoaded._enemyTarget = _tower.GetCurrentEnemyTarget();
+            _currentProjectileLoaded._enemyTarget = GetCurrentEnemyTarget();
             _nextAttkTime = Time.time + _tower.FireRate;
         }
     }
